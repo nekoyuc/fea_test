8082659f524f79cf24b7f9ca683b699d410e93ce
Yuchen Song <sycamore361@hotmail.com>
2024-07-14 21:13:18 -0700

    mesh functions solidated

--- cgx_frd.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-#with open('config.json') as f:
-#    config = json.load(f)
-
-def cgx_frd(frd_file):
-    os.system("cgx " + frd_file)
--- mesh_repairing.py
+++ mesh_repairing.py
@@ -1,9 +1,11 @@
 import pymeshlab
+from mesh_processors import check_water_tightness as cwt
 
 file_path = "Thingi10K/selected/36075.stl"
 ms = pymeshlab.MeshSet()
 ms.load_new_mesh(file_path)
 
+'''
 def check_water_tightness(ms):
     measures = ms.apply_filter("get_geometric_measures")
     # Check if key "inertia_tensor" exists in the measures dictionary. If so, mesh is watertight.
@@ -16,6 +18,7 @@ def check_water_tightness(ms):
         # Key "inertia_tensor" does not exist in the measures dictionary
         print("Mesh is not watertight")
         return False
+'''
 
 ms.apply_filter("meshing_isotropic_explicit_remeshing", adaptive = True, checksurfdist = False)
 # Check number of vertices and faces
@@ -23,5 +26,5 @@ print(ms.current_mesh().vertex_number())
 print(ms.current_mesh().face_number())
 ms.apply_filter("generate_resampled_uniform_mesh", cellsize = pymeshlab.PercentageValue(1.5), mergeclosevert = True, multisample = True)
 # Check if mesh is watertight
-check_water_tightness(ms)
+cwt(ms)
 
